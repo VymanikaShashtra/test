ea3de338317bcddcf009cb496beca1443a28567f
Hiccup Vdriod <vdriod7@gmail.com>
2017-08-12 12:34:45 +0530

    check 2

--- src/com/javacodegeeks/examples/jersey/GitCheck.java
+++ src/com/javacodegeeks/examples/jersey/GitCheck.java
@@ -5,6 +5,7 @@ public class GitCheck {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("master Check");
+		System.out.println("master Check2");
 
 	}
 
